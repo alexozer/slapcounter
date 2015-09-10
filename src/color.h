0ec136f3516d74fbc5504d35329d6cd820cc64b7
Alex Ozer <thealexozer@gmail.com>
2015-09-10 19:59:09 -0400

    Add more colors

--- src/color.h
+++ src/color.h
@@ -4,6 +4,8 @@
 #include "SmartMatrix_32x32.h"
 
 const rgb24 red = {255, 0, 0},
+	  orange = {255, 165, 0},
+	  yellow = {255, 255, 0},
 	  green = {0, 255, 0},
 	  blue = {0, 0, 255},
 	  black = {0, 0, 0};
